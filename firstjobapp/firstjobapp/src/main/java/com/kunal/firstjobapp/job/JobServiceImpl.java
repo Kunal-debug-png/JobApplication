30e11de542207c524e248b49c2e5f867f56c7bf5
Kunal Chhablani <kunalchhablani14@gmail.com>
2024-06-29 23:51:03 +0530

    Service implementation

--- firstjobapp/firstjobapp/src/main/java/com/kunal/firstjobapp/job/JobServiceImpl.java
+++ firstjobapp/firstjobapp/src/main/java/com/kunal/firstjobapp/job/JobServiceImpl.java
@@ -1,4 +1,35 @@
 package com.kunal.firstjobapp.job;
 
-public class JobService {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+@Service
+public class JobServiceImpl implements JobService{
+    private List<Job> jobs = new ArrayList<>();
+    private Long nextId=1L;
+    @Override
+    public List<Job> findAll() {
+        return jobs;
+    }
+
+    @Override
+    public void createJob(Job job) {
+        job.setId(nextId++);
+        jobs.add(job);
+    }
+
+    @Override
+    public Job geJobById(Long id) {
+        for(Job job:jobs){
+            if(job.getId().equals(id)){
+                return new ResponseEntity<>(job, HttpStatus.OK).getBody();
+            }
+        }
+        return null;
+
+    }
+
 }
