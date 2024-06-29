22f7ea2161e179540a3e8a552c297208263333ef
Kunal Chhablani <kunalchhablani14@gmail.com>
2024-06-29 23:50:15 +0530

    Job contoller

--- firstjobapp/firstjobapp/src/main/java/com/kunal/firstjobapp/job/JobController.java
+++ firstjobapp/firstjobapp/src/main/java/com/kunal/firstjobapp/job/JobController.java
@@ -1,2 +1,40 @@
-package com.kunal.firstjobapp.job;public class JobController {
+package com.kunal.firstjobapp.job;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+@AllArgsConstructor
+@RestController
+public class JobController {
+    private JobService jobServie;
+
+    @GetMapping("/jobs")
+    public List<Job> findAll(){
+        return jobServie.findAll();
+    }
+
+    @PostMapping("/jobs")
+    public String createJob(@RequestBody Job job){
+        jobServie.createJob(job);
+        return "Job added success";
+    }
+
+    @GetMapping("/jobs/{id}")
+    public Job getJobById(@PathVariable Long id){
+        Job job=jobServie.geJobById(id);
+        return job;
+    }
+
+    @DeleteMapping("/jobs/{id}")
+    public ResponseEntity<String> deleteJob(@PathVariable Long id){
+        boolean deleted=jobServie.deleteJobById(id);
+        if(deleted)
+            return new ResponseEntity<>("job delted", HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
 }
