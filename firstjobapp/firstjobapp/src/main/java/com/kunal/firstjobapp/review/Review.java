40b9a746023b18f1354f9fa3fb552efa0a7e794b
Kunal Chhablani <kunalchhablani14@gmail.com>
2024-07-02 23:57:35 +0530

    Review entity model

--- firstjobapp/firstjobapp/src/main/java/com/kunal/firstjobapp/review/Review.java
+++ firstjobapp/firstjobapp/src/main/java/com/kunal/firstjobapp/review/Review.java
@@ -1,2 +1,31 @@
-package com.kunal.firstjobapp.review;public class Review {
+package com.kunal.firstjobapp.review;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.kunal.firstjobapp.company.Company;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private double rating;
+    @JsonIgnore
+    @ManyToOne
+    private Company company;
+
+
 }
