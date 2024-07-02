ebf5c727f5558a155bcaa404d04ffeb9414efd7f
Kunal Chhablani <kunalchhablani14@gmail.com>
2024-07-02 23:58:26 +0530

    contols defined

--- firstjobapp/firstjobapp/src/main/java/com/kunal/firstjobapp/review/ReviewController.java
+++ firstjobapp/firstjobapp/src/main/java/com/kunal/firstjobapp/review/ReviewController.java
@@ -1,2 +1,39 @@
-package com.kunal.firstjobapp.review;public class ReviewController {
+package com.kunal.firstjobapp.review;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@AllArgsConstructor
+@RestController
+@RequestMapping("/companies")
+public class ReviewController {
+    ReviewService reviewService;
+
+
+    @GetMapping("/{companyId}/reviews")
+    public List<String> reviews() {
+        return null;
+    }
+
+    //2
+    @PostMapping("/{companyId}/reviews")
+    public void addReview(@PathVariable Long companyId){
+
+    }
+    //3
+    @GetMapping("/{companyId}/reviews/{reviewId}")
+    public String getReview(@PathVariable Long companyId,@PathVariable Long reviewId){
+        return null;
+    }
+    //4
+    @PutMapping("/{companyId}/reviews/{reviewId}")
+    public String updateReview(@PathVariable Long companyId,@PathVariable Long reviewId){
+        return null;
+    }
+
+    @DeleteMapping("/{companyId}/reviews/{reviewId}")
+    public String reviewById(){
+        return null;
+    }
 }
